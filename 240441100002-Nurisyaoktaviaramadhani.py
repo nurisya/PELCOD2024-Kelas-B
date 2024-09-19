c8a335fd0eec9ff3d132cedd5372a000a7a7af6f
nurisya <240441100002@student.trunojoyo.ac.id>
2024-09-19 13:37:36 +0700

    Add files via upload

---
+++ 240441100002-Nurisyaoktaviaramadhani.py
@@ -0,0 +1,10 @@
+nama = input("Siapa nama Anda? ")
+print(f"Halo, {nama}! Senang bertemu dengan Anda.")
+    
+hobi = input("Apa hobi Anda? ")
+print(f"Wow, {hobi} terdengar menyenangkan!")
+
+usia = input("Berapa usia Anda? ")
+print(f"Jadi, Anda berusia {usia} tahun. Luar biasa!")
+
+print("Terima kasih telah berbagi dengan kami. Sampai jumpa!")
